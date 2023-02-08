e5be99ae6f19b545611f70a22f07ff99863fcb87
gao <gao@gmail.com>
2023-02-08 19:58:40 +0800

    feat: Add Github Action

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿await Bootstrapper
     .Factory
     .CreateWeb(args)
+    .DeployToGitHubPages(
+        "myblog", //修改成自己的帳號
+        "myblog.github.io", //修改成自己的帳號
+        Config.FromSetting<string>("BLOG_TOKEN") //之後建立一個新的Github Token
+    )
     .RunAsync();
